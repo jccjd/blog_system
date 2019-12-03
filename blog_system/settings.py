4324bba5613713be48a16dc1c4db841c715bbb50
jccd <1340944538@qq.com>
2019-12-03 14:15:16 +0800

    add

--- blog_system/settings.py
+++ blog_system/settings.py
@@ -29,8 +29,7 @@ ALLOWED_HOSTS = []
 # Application definition
 import dj_database_url
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-
+DATABASES = { 'default': dj_database_url.config() }
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
