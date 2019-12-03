68cf8fe2fc1707d1895bfc9279cf3d9aaf071a36
jccd <1340944538@qq.com>
2019-12-03 14:08:43 +0800

    new

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn blog_system.wsgi --log-file -
--- blog_system/settings.py
+++ blog_system/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 # Application definition
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -116,4 +119,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
