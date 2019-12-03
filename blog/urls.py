256aee628ba74d24b13c9e9deda7f431d818bd9c
jccd <1340944538@qq.com>
2019-12-03 11:34:46 +0800

    change error

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from blog import views
 
-urlpatterns = [21
+urlpatterns = [
     path(r'', views.get_blogs),
     path(r'<int:blog_id>', views.get_details, name='detail')
 ]
